373572c5437653c256a74bd06f43318956e6b0b2
Danila <syvs0990@mail.ru>
2023-01-17 23:00:06 +0700

    Implemented rook, bishop and queen moves

--- src/app/modules/chess/services/chess-moves.service.ts
+++ src/app/modules/chess/services/chess-moves.service.ts
@@ -43,7 +43,7 @@ export class ChessMovesService {
   }
 
   public calcAllowedMoves(currentFieldIndex: number, fields: Field[]) {
-    if (!currentFieldIndex || !fields || !this.indexRange.includes(currentFieldIndex)) { 
+    if (isNil(currentFieldIndex) || !fields || !this.indexRange.includes(currentFieldIndex)) { 
       return;
     }
 
@@ -51,7 +51,16 @@ export class ChessMovesService {
 
     switch(fields[currentFieldIndex].piece?.type) {
       case PieceType.pawn:
-        moves = [...this.calcPawnMoves(currentFieldIndex, fields)]
+        moves = [...this.calcPawnMoves(currentFieldIndex, fields)];
+        break;
+      case PieceType.rook:
+        moves = [...this.calcRookMoves(currentFieldIndex, fields)];
+        break;
+      case PieceType.bishop:
+        moves = [...this.calcBishopMoves(currentFieldIndex, fields)];
+        break;
+      case PieceType.queen:
+        moves = [...this.calcQueenMoves(currentFieldIndex, fields)];
         break;
       default:
         moves = [];
@@ -78,7 +87,7 @@ export class ChessMovesService {
     const cutRightIndex = white ? index - 7 : index + 9;
     if (
       fields[cutRightIndex]?.piece?.color !== fields[index].piece?.color
-      && fields[cutRightIndex]?.piece
+      && !!fields[cutRightIndex]?.piece
       && (index + 1) % 8 !== 0
     ) {
       moves.push(cutRightIndex)
@@ -87,7 +96,7 @@ export class ChessMovesService {
     const cutLeftIndex = white ? index - 9 : index + 7;
     if (
       fields[cutLeftIndex]?.piece?.color !== fields[index].piece?.color
-      && fields[cutLeftIndex]?.piece
+      && !!fields[cutLeftIndex]?.piece
       && index % 8 !== 0
     ) {
       moves.push(cutLeftIndex)
@@ -95,4 +104,82 @@ export class ChessMovesService {
 
     return moves;
   }
+
+  private calcRookMoves(index: number, fields: Field[]): number[] {
+    const moves: number[] = [];
+
+    for (let i = index + 8; i < 64; i += 8) {
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+    }
+
+    for (let i = index - 8; i >= 0 ; i -= 8) {
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+    }
+
+    for (let i = index + 1; i < 64; i++) {
+      if ((index + 1) % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if ((i + 1) % 8 === 0) { break; }
+    }
+
+    for (let i = index - 1; i >= 0; i--) {
+      if (index % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if (i % 8 === 0) { break; }
+    }
+
+    return moves;
+  }
+
+  private calcBishopMoves(index: number, fields: Field[]): number[] {
+    const moves: number[] = [];
+
+    for (let i = index - 7; i >= 0; i -= 7) {
+      if ((index - 7) % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if ((i - 7) % 8 === 0) { break; }
+    }
+
+    for (let i = index - 9; i >= 0; i -= 9) {
+      if (index % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if (i % 8 === 0) { break; }
+    }
+
+    for (let i = index + 7; i < 64; i += 7) {
+      if (index % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if (i % 8 === 0) { break; }
+    }
+
+    for (let i = index + 9; i < 64; i += 9) {
+      if ((index + 9) % 8 === 0) { break; }
+      if (this.dynamicCalc(moves, fields, index, i)) { break; }
+      if ((i + 9) % 8 === 0) { break; }
+    }
+
+    return moves;
+  }
+
+  private calcQueenMoves(index: number, fields: Field[]): number[] {
+    return [...this.calcBishopMoves(index, fields), ...this.calcRookMoves(index, fields)];
+  }
+
+  private dynamicCalc(
+    moves: number[],
+    fields: Field[],
+    currentFieldIndex: number,
+    fieldIndex: number
+  ): boolean {
+    if (!!fields[fieldIndex].piece) {
+      if (fields[fieldIndex].piece?.color !== fields[currentFieldIndex].piece?.color) {
+        moves.push(fieldIndex);
+      }
+      return true;
+    }
+    moves.push(fieldIndex);
+    return false;
+  }
+
+
 }
