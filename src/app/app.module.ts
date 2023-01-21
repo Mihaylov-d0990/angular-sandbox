8d85c8ce477ac7d1e910668c686d4c9de7d96b65
Danila <syvs0990@mail.ru>
2023-01-21 22:27:25 +0700

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChessModule } from './modules/chess/chess.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ChessModule } from './modules/chess/chess.module';
     BrowserModule,
     AppRoutingModule,
     ChessModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
