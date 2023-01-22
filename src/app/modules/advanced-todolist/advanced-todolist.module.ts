3d0ab734769bb4304435fe682fa597823c00af28
Danila <syvs0990@mail.ru>
2023-01-22 22:54:24 +0700

    Generated date-picker and todolist components

--- src/app/modules/advanced-todolist/advanced-todolist.module.ts
+++ src/app/modules/advanced-todolist/advanced-todolist.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvancedTodolistComponent } from './advanced-todolist.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 
 
 
 @NgModule({
   declarations: [
-    AdvancedTodolistComponent
+    AdvancedTodolistComponent,
+    TodolistComponent,
+    DatePickerComponent
   ],
   imports: [
     CommonModule
